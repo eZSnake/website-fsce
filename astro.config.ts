67faf568ff17ca009e821dbe05348d6b3b39b7af
Carsten Wesp <carsten.wesp@stud.tu-darmstadt.de>
2024-07-05 17:48:13 +0200

    Revert "does this link other pages"
    This reverts commit 775d1fb55ec3ade7dd78a0b9623ac455c54fc1e9.

--- astro.config.ts
+++ astro.config.ts
@@ -35,8 +35,8 @@ const autolinkOptions: rehypeAutolinkOptions = {
 };
 
 export default defineConfig({
-	site: "https://castn.github.io/website-fsce/",
-	base: "/",
+	site: "https://castn.github.io/",
+	base: "website-fsce",
 	trailingSlash: SITE.trailingSlash ? 'always' : 'never',
 
 	output: 'static',
