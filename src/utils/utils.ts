6d42b65fc4f06de9dcca4b652f5cfb56d8f8d914
Carsten Wesp <carsten.wesp@stud.tu-darmstadt.de>
2024-07-18 21:55:19 +0200

    add static calendar

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,8 +2,19 @@
 export const getFormattedDate = (date: string | number | Date) =>
 	date
 		? new Date(date).toLocaleDateString('en-gb', {
-				year: 'numeric',
-				month: 'long',
-				day: 'numeric',
-			})
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric',
+		})
 		: '';
+
+export const getLocationOnMap = (location: string) => {
+	return 'https://maps.google.com/?q=' + location;
+};
+
+import { format } from 'date-fns';
+
+export const convertTimestamp2Date = (timestamp: number, dateFormat: string) => {
+	const date = new Date(timestamp);
+	return format(date, dateFormat);
+};
